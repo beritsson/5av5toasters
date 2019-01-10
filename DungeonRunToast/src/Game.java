4c5f2e289efab2e685a0faaa01586602c972290d
ThomasT <thomastalving@thomas-macbook-pro.local>
2019-01-10 15:58:38 +0100

    New menu.

--- DungeonRunToast/src/Game.java
+++ DungeonRunToast/src/Game.java
@@ -1,78 +1,215 @@
-
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Scanner;
+
 
 public class Game {
 	
 	static Scanner input = new Scanner(System.in);
 	
-	public static void main(String[] args) {
-		
-		boolean loggedIn = false;
-		
-		while(!loggedIn) {
-			System.out.println("\tWelcome to Toastmaster Dungeon");
-			System.out.println("\t[N]ew Game");
-			System.out.println("\t[L]oad game");
-			System.out.println("\t[E]xit game");
-			System.out.print("\tEnter: ");
-			String choice = input.next().toLowerCase();
-			
-			switch (choice) {
-			case "n":
-				System.out.print("\tEnter you heros name: ");
-				String userName = input.next();
-				System.out.println("\tWhat hero do you want to be? \n\t[K]night [W]izard [T]hief");
-				System.out.print("\tEnter: ");
-				String heroChoice = input.next().toLowerCase();
-				switch (heroChoice) {
-				case "k":
-					Character heroK = new Knight(userName);
-					heroK.characterAttribute();
-					loggedIn = true;
-					break;
-				case "w":
-					Character heroW = new Wizard(userName);
-					heroW.characterAttribute();
-					loggedIn = true;
-					break;
-				case "t":
-					Character heroT = new Thief(userName);
-					heroT.characterAttribute();
-					loggedIn = true;
-				default:
-					break;
+	public static void main(String[] args) { 
+	
+		while (true) {
+			menulogo();
+			System.out.println(
+							  "     											                \n"			
+							+ "					 		  	╔══════════════════════════════════╗\n"
+							+ "							  	║              GAME MENU           ║\n"
+							+ "							  	╚══════════════════════════════════╝\n"
+							+ "							  	╔══════════════════════════════════╗\n"
+							+ "							  	║                                  ║\n"
+							+ "							  	║             [N]ew Game           ║\n"
+							+ "							  	║                                  ║\n"
+							+ "							  	║             [L]oad Game          ║\n"
+							+ "							  	║                                  ║\n"
+							+ "							  	║              [E]xit              ║\n"
+							+ "							  	║                                  ║\n"
+							+ "							  	╚══════════════════════════════════╝\n"
+							+"TEST"
+
+					);
+			Scanner scanner = new Scanner(System.in);
+			String choice =scanner.next();
+
+			if (choice.equalsIgnoreCase("N")) { // START GAME
+				System.out.println("NEW GAME");
+				newgamemenu();
+				
+			} else if (choice.equalsIgnoreCase("L")) { // LOAD GAME
+				loadmenu();
+				
+			} else if (choice.equalsIgnoreCase("E")) { // EXIT
+				System.out.println("Have a nice day!");
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					System.out.printf("Somthing went wrong:(", e);
+					e.printStackTrace();
 				}
-				chooseMap();
-				break;
-			case "l":
-				break;
-			case "e":
-				System.out.println("\tWelcome back!");
+				System.out.println("\tBye....");
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					System.out.printf("Somthing went wrong:(", e);
+					e.printStackTrace();
+				}
+				System.out.println("\t\tBye....");
 				System.exit(0);
-			default:
-				System.out.println("\tNot a valid choice, try again");
-				break;
+			} else if (choice.equalsIgnoreCase("A")) {
+			} else if (!choice.equalsIgnoreCase("A") && !choice.equalsIgnoreCase("S")
+					&& !choice.equalsIgnoreCase("C")) {
+				System.out.println("Invalid choice, returning to Menu...\n");
+				
 			}
+
+
+			
 		}
 		
 	}
 	
-	static void chooseMap(){
-		System.out.print("\tChoose your map: \n\t[S]mall [M]edium [B]ig ");
-		System.out.print("\tEnter: ");
-		String map = input.next().toLowerCase();
-		switch (map) {
-		case "s":
-			Maps(4);
-			break;
-		case "m":
-			Maps(5);
-			break;
-		case "b":
-			Maps(8);
-		default:
-			break;
+	public static void loadmenu() {
+		
+		System.out.println(
+				  "     											     \n"			
+				+ "					 		  	╔══════════════════════════════════╗\n"
+				+ "							  	║         PLEASE ENTER NAME        ║\n"
+				+ "							  	╚══════════════════════════════════╝\n"
+
+		);
+		
+		Scanner inputPName = new Scanner(System.in);
+		String pname =inputPName.next();
+		 
+		if (pname.equalsIgnoreCase("PLAYERS USERNAME FOUND")) { //----------------------------------------------------INSERT CHECK IF PLAYER NAME EXIST 
+			System.out.println("You're load has been found");
+			//Start game EASY MAP
+			
+		} else  { // MEDIUM
+			System.out.println("Error not found");
+			return;
+
+	}
+	}
+
+public static void menulogo() {
+	System.out.println("\n\n\n\n");
+
+	int width = 150;
+	int height = 30;
+
+	BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	Graphics g = image.getGraphics();
+	g.setFont(new Font("SansSerif", Font.PLAIN, 17));
+
+	Graphics2D graphics = (Graphics2D) g;
+	graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+	graphics.drawString("Toast Adventure", 15, 20);
+
+	for (int y = 0; y < height; y++) {
+		StringBuilder sb = new StringBuilder();
+		for (int x = 0; x < width; x++) {
+
+			sb.append(image.getRGB(x, y) == -16777216 ? " " : "$");
+
 		}
+
+		if (sb.toString().trim().isEmpty()) {
+			continue;
+		}
+
+		System.out.println(sb);
+	}
+}
+
+public static void newgamemenu() {
+	
+	System.out.println(
+			  "     											     \n"			
+			+ "					 		  	╔══════════════════════════════════╗\n"
+			+ "							  	║         PLEASE ENTER NAME        ║\n"
+			+ "							  	╚══════════════════════════════════╝\n"
+
+	);
+	Scanner inputPName = new Scanner(System.in);
+	String pname =inputPName.next();
+	
+	
+	System.out.println(
+			  "                                                                                                                   \n"			
++ "                                                                ╔════════════════════════════════════╗                                      \n"
++ "                                                                ║         CHOOSE A CHARACTER         ║                                      \n"
++ "                                                                ╚════════════════════════════════════╝                                      \n"
++ "                          ╔════════════════════════════════════╗╔════════════════════════════════════╗╔════════════════════════════════════╗\n"
++ "                          ║             [K]night               ║║              [W]izard              ║║               [T]hife              ║\n"
++ "                          ║                                    ║║                                    ║║                                    ║\n"
++ "                          ║   Special abillites: Shieldblock   ║║   Special abillites: Light Rail    ║║   Special abillites: Critical Hit  ║\n"
++ "                          ║                                    ║║                                    ║║                                    ║\n"
++ "                          ║           Initiative: 5            ║║           Initiative: 6            ║║           Initiative: 7            ║\n"
++ "                          ║           Health: 9                ║║           Health: 4                ║║           Health: 5                ║\n"
++ "                          ║           Attack: 6                ║║           Attack: 9                ║║           Attack: 5                ║\n"
++ "                          ║           Flexibility: 4           ║║           Flexibility: 5           ║║           Flexibility: 7           ║\n"
++ "                          ║                                    ║║                                    ║║                                    ║\n"
++ "                          ║                                    ║║                                    ║║                                    ║\n"
++ "                          ╚════════════════════════════════════╝╚════════════════════════════════════╝╚════════════════════════════════════╝\n"
+
+	);
+	Scanner inputcharacter = new Scanner(System.in);
+	String character =inputcharacter.next();
+	
+	if (character.equalsIgnoreCase("K")) { // KNIGHT
+		System.out.println("You have choosen the Knight");
+
+		
+	} else if (character.equalsIgnoreCase("W")) { // WIZARD
+		System.out.println("You have choosen the Wizard");
+
+		
+	} else if (character.equalsIgnoreCase("T")) { // THIEF
+		System.out.println("You have choosen the Thife");
+	}
+	mapmenu();
+}
+
+
+	
+	static void mapmenu(){
+		
+		System.out.println(
+				  "     											     \n"			
+							+ "					 		  	╔═════════════════════════════════════╗\n"
+							+ "							  	║       Choose you're diffuculity     ║\n"
+							+ "							  	╚═════════════════════════════════════╝\n"
+							+ "					 		  	╔═══════════╗╔═══════════╗╔═══════════╗\n"
+							+ "							  	║   [E]asy  ║║ [M]edium  ║║  [H]ard   ║\n"
+							+ "							  	║  Map 4x4  ║║  Map 5x5  ║║  Map 8x8  ║\n"
+							+ "							  	╚═══════════╝╚═══════════╝╚═══════════╝\n"
+
+		);
+		
+		Scanner inputdiff = new Scanner(System.in);
+		String diffuculity =inputdiff.next();
+		
+		if (diffuculity.equalsIgnoreCase("E")) { // EASY
+			System.out.println("You have choosen EASY");
+
+			//Start game EASY MAP
+			
+		} else if (diffuculity.equalsIgnoreCase("M")) { // MEDIUM
+			System.out.println("You have choosen MEDIUM");
+
+			//Start game MEDIUM MAP
+			
+		} else if (diffuculity.equalsIgnoreCase("H")) { // HARD
+			System.out.println("You have choosen HARD");
+			//Start game HARD MAP
+		}
+		
+		
 	}
 
 	static Room[][] Maps(int x){
