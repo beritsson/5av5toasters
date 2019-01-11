85883743fd4e2829e8125a2e4eb27d1698a87f77
limengran1029 <mengran.li@yh.nackademin.se>
2019-01-11 11:12:02 +0100

    Revert "Created boolean monsterinroom(); in room class"
    This reverts commit 937e061945410d8ffb2ec5ca325ea69710cb9462.

--- DungeonRunToast/src/Monster.java
+++ DungeonRunToast/src/Monster.java
@@ -2,150 +2,76 @@ import java.util.ArrayList;
 
 public class Monster {
 
-
-
 	protected int initiativ;
-
 	protected int resistance;
-
 	protected int attack;
-
 	protected int flexibility;
-
 	protected int commonness;
 
-
-
 	void attributes() 
-
 	{
-
 		System.out.println("-------Creatures Attribute-------");
-
 		System.out.println("\tInitiativ: " + initiativ);
-
 		System.out.println("\tResistance: " + resistance);
-
 		System.out.println("\tAttack: " + attack);
-
 		System.out.println("\tFlexibility: " + flexibility);
-
 		System.out.println("\tCommoness: " + commonness + "%");
-
 	}
-
-
 
 	public void showAttribute(ArrayList<Monster> monsterList)
-
 	{
-
 		for (Monster monster : monsterList) {
 
-
-
 			monster.attributes();
-
 		}
-
 	}
-
-
 
 	public int getInitiativ() {
-
 		return initiativ;
-
 	}
-
-
 
 	public void setInitiativ(int initiativ) {
-
 		this.initiativ = initiativ;
-
 	}
-
-
 
 	public int getResistance() {
-
 		return resistance;
-
 	}
-
-
 
 	public void setResistance(int resistance) {
-
 		this.resistance = resistance;
-
 	}
-
-
 
 	public int getAttack() {
-
 		return attack;
-
 	}
-
-
 
 	public void setAttack(int attack) {
-
 		this.attack = attack;
-
 	}
-
-
 
 	public int getFlexibility() {
-
 		return flexibility;
-
 	}
-
-
 
 	public void setFlexibility(int flexibility) {
-
 		this.flexibility = flexibility;
-
 	}
-
-
 
 	public int getCommonness() {
-
 		return commonness;
-
 	}
-
-
 
 	public void setCommonness(int commonness) {
-
 		this.commonness = commonness;
-
 	}
-
 	
-
 	
-
 	ArrayList<Monster> addMonster(ArrayList<Monster> monsterList){
-
 		if (this.commonness > (int)(Math.random()*100)){
-
 			monsterList.add(this);
-
 		}	
-
 		return monsterList;
-
 	}
-
-
 
 }
